d033bdceec8949958c146d0ea829ef5bce9c1e79
Rahel Zegeye <rahelzegeye21@outlook.com>
2024-11-25 13:07:43 +0300

    Banner

--- Netfilix-Rahel/src/Page/Home/Home.jsx
+++ Netfilix-Rahel/src/Page/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Header from '../../component/Header/Header'
+ import Banner from "../../component/Banner/Banner"
 import Footer from '../../component/Footer/Footer'
 
 
@@ -8,6 +9,7 @@ const Home = () => {
   return (
     <>
     <Header/>
+ <Banner/> 
     <Footer/>
     
     
