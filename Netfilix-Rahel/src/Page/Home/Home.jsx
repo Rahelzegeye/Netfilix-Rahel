6dc1cc78fad65e8efd3381be7a6feb000fcf448a
Rahel Zegeye <rahelzegeye21@outlook.com>
2024-11-25 16:24:24 +0300

    Row and Rowlist

--- Netfilix-Rahel/src/Page/Home/Home.jsx
+++ Netfilix-Rahel/src/Page/Home/Home.jsx
@@ -3,13 +3,15 @@ import React from 'react'
 import Header from '../../component/Header/Header'
  import Banner from "../../component/Banner/Banner"
 import Footer from '../../component/Footer/Footer'
+import RowList from '../../component/Rows/RowList/RowList'
 
 
 const Home = () => {
   return (
     <>
     <Header/>
- <Banner/> 
+    <Banner/> 
+    <RowList/>
     <Footer/>
     
     
